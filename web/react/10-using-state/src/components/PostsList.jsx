a33f716c3af48f91749933aeba88ab566830e930
Hylson <hvescovi@gmail.com>
2026-01-08 17:18:02 -0300

    chore:using state

---
+++ web/react/10-using-state/src/components/PostsList.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';   
+
+import Post from './Post';
+import classes from './PostsList.module.css';   
+import NewPost from './NewPost';
+
+function PostsList() {
+    
+    const [enteredBody, setEnteredBody] = useState('');  
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+    
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value);
+    }
+    function authorChangeHandler(event){
+        setEnteredAuthor(event.target.value);
+    }
+
+    return (
+        <>
+        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+        <ul className={classes.posts}>
+            <Post className={classes.post} author={enteredAuthor} body={enteredBody} />
+            <Post className={classes.post} author="Manuel"  body="I love programming" />
+        </ul>
+        </>
+    );
+}
+
+export default PostsList;
