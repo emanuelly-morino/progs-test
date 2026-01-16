b479ff13a7362e766ad8b8e9411ffaa34a67443d
Hylson <hvescovi@gmail.com>
2026-01-15 21:11:46 -0300

    feat:global header

---
+++ web/react/13-header/src/components/PostsList.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+import Post from './Post';
+import classes from './PostsList.module.css';
+import NewPost from './NewPost';
+import Modal from './Modal';
+
+function PostsList({ isPosting, onStopPosting }) {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    // condition && code; code will be executed only if condition is true
+
+    return (
+        <>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                    <NewPost
+                        onBodyChange={bodyChangeHandler}
+                        onAuthorChange={authorChangeHandler} />
+                </Modal> ) 
+            } 
+            <ul className={classes.posts}>
+                <Post className={classes.post} author={enteredAuthor} body={enteredBody} />
+                <Post className={classes.post} author="Manuel" body="I love programming" />
+            </ul>
+
+        </>
+    );
+}
+
+export default PostsList;
