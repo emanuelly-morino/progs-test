3a2e1be915b2521433fa1d9b8c79dc6e976b2ba4
Hylson <hvescovi@gmail.com>
2026-03-23 09:17:43 -0300

    chore:renumbering

--- web/js/08-biblioteca.js
+++ web/js/08-biblioteca.js
@@ -5,7 +5,7 @@ export const PI = 3.14159;
 export function somar(a, b) {
   return a + b;
 }
-// exportação “padrão” - deve haver somente 1 por módulo
+// exportação "padrão" - deve haver somente 1 por módulo
 export default function multiplicar(a, b) {
   return a * b;
 }
