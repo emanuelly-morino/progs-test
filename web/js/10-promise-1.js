ced59d8db6e8e956f59df7eb4e3ee4b964c3e84c
Hylson <hvescovi@gmail.com>
2026-03-22 21:20:08 -0300

    doc:adding comments to the promise/await/callback

--- web/js/10-promise-1.js
+++ web/js/10-promise-1.js
@@ -1,23 +1,39 @@
 
 function sorteiaUmNumero() {
+    // cria um objeto do tipo Promise
     return new Promise(
+        // a função sorteia é a função de callback que deve ser chamada quando o número for sorteado
+        // o número sorteado deve ser passado como argumento para a função sorteia
+        // essa função será chamada dentro do bloco da função com seta (Arrow Function)
         (sorteia) => {
-            setTimeout(() => {
+            // é criado um temporizador
+            setTimeout(
+                // a função com seta é a função de callback que 
+                // deve ser chamada quando o temporizador expirar
+                () => {
                 let n1 = Math.random();
                 let n2 = n1 * 1000;
                 let n3 = parseInt(n2);
                 sorteia(n3);
-            }, 1000);
+            }, 
+            // o temporizador é configurado 
+            // para expirar após 1000 milissegundos (1 segundo)
+            1000);
         });
 }
 
-// locate the element on the screen
+// localiza o elemento na tela
 const element1 = document.getElementById("numbers");
 
 for (let i = 0; i < 10; i++) {
-    // get a random number
+    // obtém um número aleatório
+    // como a função sorteiaUmNumero retorna uma Promise, 
+    // é necessário usar a palavra-chave await para 
+    // esperar que a Promise seja resolvida e 
+    // obter o número sorteado
     let n = await sorteiaUmNumero();
-    // put the number in the screen
+    // colocar o número sorteado na tela
     element1.textContent += `${n}, `;
 
 }
+
