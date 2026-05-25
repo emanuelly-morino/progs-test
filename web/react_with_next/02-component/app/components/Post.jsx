0ced5d403d7763111b74365bd1ece28ccc24f75b
Hylson <hvescovi@gmail.com>
2026-05-25 07:56:49 -0300

    docs:improving first component documentation

--- web/react_with_next/02-component/app/components/Post.jsx
+++ web/react_with_next/02-component/app/components/Post.jsx
@@ -1,7 +1,13 @@
+// o componente precisa ficar dentro de uma função
 function Post() {
+    // a função precisa retornar um código HTML
     return (
         <div>
             <h1>Olá este é um post</h1>
         </div>
     );
 }
+
+// deixando o componente apto para ser usado
+// externamente, ou seja, em outros arquivos
+export default Post;
